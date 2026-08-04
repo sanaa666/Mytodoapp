4ab9d463d0cd9f9956d5855ec3b6e1590bd7ee14
sanaa666 <sanaafarhaan333@gmail.com>
2026-08-04 05:54:00 +0100

    fix: remove unnecessary div wrapper around Add button for cleaner markup

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,11 +1,9 @@
 export default function AddButton({ addItem }) {
   return (
-    <div>
     <button
       className="add-button"
       onClick={addItem}>
       Add
     </button>
-    </div>
   );
 }
